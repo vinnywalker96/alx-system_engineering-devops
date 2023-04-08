4c0dcaf44379817f8b432a6d6b3eec5b954a7d68
Marlvin Kwenda <vinnywalker96>
2023-04-08 05:14:09 -0700

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,73 +1,24 @@
-# Install Nginx
-class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
+# Script to install nginx using puppet
+
+package {'nginx':
+  ensure => 'present',
 }
 
-# Configure Nginx server
-class nginx::config {
-  # Configure Nginx to listen on port 80
-  file { '/etc/nginx/sites-available/default':
-    ensure => file,
-    content => "
-server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
-
-  root /var/www/html;
-  index index.html index.htm index.nginx-debian.html;
-
-  server_name _;
-
-  location / {
-    # Return 'Hello World!' on GET request to /
-    if ($request_method = 'GET') {
-      add_header Content-Type text/html;
-      return 200 'Hello World!';
-    }
-  }
-
-  # Redirect /redirect_me to /
-  location /redirect_me {
-    return 301 /;
-  }
-}
-",
-    notify => Service['nginx'],
-  }
-
-  # Remove default Nginx welcome page
-  file { '/var/www/html/index.nginx-debian.html':
-    ensure => absent,
-  }
-
-  # Create a placeholder index.html file
-  file { '/var/www/html/index.html':
-    ensure => file,
-    content => 'This is the default index.html file',
-  }
-
-  # Enable the default site
-  file { '/etc/nginx/sites-enabled/default':
-    ensure => 'link',
-    target => '/etc/nginx/sites-available/default',
-    notify => Service['nginx'],
-  }
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
 }
 
-# Restart Nginx
-class nginx::service {
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
+exec {'Hello World!':
+  command  => 'echo "Hello World!" | sudo dd status=none of=/var/www/html/index.html',
+  provider => shell,
 }
 
-# Apply classes to node
-node default {
-  include nginx
-  include nginx::config
-  include nginx::service
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/https://www.youtube.com/watch?v=QH2-TGUlwu4\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
 }
 
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
