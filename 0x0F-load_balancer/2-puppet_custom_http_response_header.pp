ec9214690ec66edfa75864c2cbf662bf3097f466
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-10 07:18:32 -0700

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,71 +1,29 @@
-#custom HTTP header with Puppet
-class nginx_custom_header {
-  package { 'nginx':
-    ensure => installed,
-  }
+# Configure HTTP header in a nginx server
 
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
-
-  file { '/var/www/html/index.html':
-    content => 'Hello World!',
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-  }
-
-  file { '/var/www/html/404.html':
-    content => "Ceci n'est pas une page",
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-  }
-
-  file { '/etc/nginx/sites-enabled/default':
-    content => template('nginx_custom_header/nginx.conf.erb'),
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    notify  => Service['nginx'],
-  }
-
-  file { '/etc/nginx/nginx.conf':
-    content => template('nginx_custom_header/nginx.conf.erb'),
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    notify  => Service['nginx'],
-  }
-
-  firewall { 'Nginx HTTP':
-    port   => '80',
-    proto  => 'tcp',
-    action => 'accept',
-  }
+# update ubuntu server
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
 }
-server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
-  root /var/www/html;
-  index index.html index.htm index.nginx-debian.html;
-  server_name _;
-
-  location / {
-    try_files $uri $uri/ =404;
-  }
-
-  if ($request_filename ~ redirect_me) {
-    rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-  }
-
-  error_page 404 /404.html;
-
-  location = /404.html {
-    internal;
-  }
-
-  add_header X-Served-By <%= @hostname %>;
+->
+# install nginx web server on server
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
 }
-
+->
+# custom Nginx response header (X-Served-By: hostname)
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
+}
+->
+# start service
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
+}
