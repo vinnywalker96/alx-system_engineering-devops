794d5f5b9b67f5d80aaf00eb0ce3e5b3232b7bc7
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-01 15:41:20 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -25,15 +25,17 @@ def get_employee_todo_progress(employee_id):
     TOTAL_NUMBER_OF_TASKS = len(todo_data)
 
     # Display employee TODO list progress
-    print(f"Employee {EMPLOYEE_NAME} is done\
-           with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-    print(f"\t{EMPLOYEE_NAME}: {NUMBER_OF_DONE_TASKS}\
-            completed tasks out of {TOTAL_NUMBER_OF_TASKS}")
+    print("Employee {} is done with tasks({}/{}):".
+          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, 
+          TOTAL_NUMBER_OF_TASKS))
+    print("\t{}: {} completed tasks out of {}".
+          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
+          TOTAL_NUMBER_OF_TASKS))
 
     # Display the titles of completed tasks
     print("Completed tasks:")
     for task in completed_tasks:
-        print(f"\t\t{task['title']}")
+        print(f"\t{task['title']}")
 
 
 # Example usage: get TODO list progress for employee with ID 1
