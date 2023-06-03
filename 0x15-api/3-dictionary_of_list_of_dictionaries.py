bc7c9f2003f3bf1254275698394c17901aa10210
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-03 16:48:22 +0000

    3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import json
+import requests
+
+
+def get_all_task():
+    """Retrieves all employees task"""
+    base_url = 'https://jsonplaceholder.typicode.com'
+    response = requests.get(f'{base_url}/users')
+    users = response.json()
+    res = requests.get(f"{base_url}/todos")
+    todos = res.json()
+    filename = 'todo_all_employees.json'
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump({user.get('id'): [{"username": user.get("username"),
+                  "task": todo.get("title"),
+                  "completed": todo.get("completed")
+                  } for todo in todos]
+                  for user in users}, f)
+
+
+if __name__ == "__main__":
+    get_all_task()
