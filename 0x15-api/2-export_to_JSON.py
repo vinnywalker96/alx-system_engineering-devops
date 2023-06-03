715b98a0e5240814edd070056e553f05bec64808
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-03 14:14:46 +0000

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,25 +8,27 @@ import json
 def export_to_json(employee_id):
     """exports to json format"""
     base_url = 'https://jsonplaceholder.typicode.com'
-    try:
-        #get employee information
-        reponse = requests.get(f'{base_url}/users{employee_id}')
-        employee_data = response.json()
-        employee_name = employee_data['name']
-        todo = requests.get(f'{base_url}/todos?userId={employee_id}')
-        todo_data = todo_response.json()
-        task = {}
-        filename = ""
-        for todo in todo_data:
-            if str(todo['userId']) == employee_id:
-                task['userId'] = [{
-                    "task": todo['title'],
-                    "completed": todo['completed'],
-                    "username": employee_name}]
-                filename = f"{todo['userId']}.json"
-        
-        with open(filename, 'w', encoding='utf-8') as f:
-            json.dumb(task,f) 
+    response = requests.get(f'{base_url}/users/{employee_id}')
+    data = response.json()
+    employee_name = data['name']
+    res = requests.get(f'{base_url}/todos?userId={employee_id}')
+    todos = res.json()
+    employee_task = []
+    filename = f'{employee_id}.json'
+    for todo in todos:
+        if str(todo['userId']) == employee_id:
+            task = {
+                "task": todo['title'],
+                "completed": todo['completed'],
+                "username": employee_name
+            }
+            employee_task.append(task)
+    data = {
+        employee_id: employee_task
+    }
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
 
 
 if __name__ == "__main__":
