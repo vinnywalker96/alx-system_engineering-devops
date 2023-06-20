cdb70d599947d56b06b071881ff751d618e1cff6
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-20 13:37:13 +0200

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+""" Import Libraies"""
 import requests
 
+
 def get_subreddit_subscribers(subreddit):
+    """_queries the Reddit API and returns the number of subscribers 
+
+    Args:
+        subreddit (string): _description_
+
+    Returns:
+        int: number of subscribers 
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Custom User Agent'}
     response = requests.get(url, headers=headers)
