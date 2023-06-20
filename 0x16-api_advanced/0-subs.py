acccc5e397e4973bb539473a0c6390e4e1f8d865
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-20 13:34:32 +0200

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import requests
+
+def get_subreddit_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
